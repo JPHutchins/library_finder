6a80cd1543ff77bc88b9b8727b19ec4f5cdc0227
J.P. Hutchins <jphutchins@gmail.com>
2020-04-08 20:25:56 -0700

    Fix indentation

--- library_finder/script.js
+++ library_finder/script.js
@@ -1,11 +1,11 @@
 document.addEventListener('click', (e) => {
-        if (!e.target) return;
-        if (e.target && e.target.nodeName != "LI") return;
-        e.stopPropagation(); 
-		const items = e.target.children; 
-		for (const item of items) { 
-			if (item === undefined) continue; 
-			if (item.tagName === "LI") continue; 
-			if (item.style.display === "block") item.style.display = "none"; 
-				else item.style.display = "block"; };
-    }); 
+    if (!e.target) return;
+    if (e.target && e.target.nodeName != "LI") return;
+    e.stopPropagation(); 
+	const items = e.target.children; 
+	for (const item of items) { 
+		if (item === undefined) continue; 
+		if (item.tagName === "LI") continue; 
+		if (item.style.display === "block") item.style.display = "none"; 
+			else item.style.display = "block"; };
+}); 
